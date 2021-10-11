a2cb90b9d284426e22fd9098bdeca710d25e144b
trungthanh <trungthanhnt@gmail.com>
2021-10-11 14:52:33 +0700

    add build script

--- IDO/SaleFixedPrice/Sale.sol
+++ IDO/SaleFixedPrice/Sale.sol
@@ -2,8 +2,8 @@
 
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+import "./IERC20Metadata.sol";
+import "./Ownable.sol";
 
 interface ITokenSale{
     /**
---
+++ IDO/SaleFixedPrice/build.sh
@@ -0,0 +1,6 @@
+/opt/node-v12.13.0-linux-x64/bin/solcjs --base-path . --optimize --bin --abi ./Sale.sol -o ./TKSale/
+cd ./TKSale/
+#../../../abigen --abi ./_TimeLockFactory_sol_TimeLockedWalletFactory.abi  --bin _TimeLockFactory_sol_TimeLockedWalletFactory.bin --pkg TKTimeLockFactoryV3 --out ../TKTimeLockFactoryV3_sol.go
+
+#../../../abigen --abi ./_TimeLock_sol_TimeLock.abi  --bin _TimeLock_sol_TimeLock.bin --pkg TKTimeLockV3 --out ../TKTimeLockV3/TKTimeLock_sol.go
+
